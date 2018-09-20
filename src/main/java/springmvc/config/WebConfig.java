f8d635bc1cc01b4613fa507a1e243f0d68a0b2c9
Briyan Adityatama <briyansykes@yahoo.com>
2018-09-20 17:49:45 +0700

    add package springmvc.config and class WebConfig.java

---
+++ src/main/java/springmvc/config/WebConfig.java
@@ -0,0 +1,74 @@
+package springmvc.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "springmvc" })
+public class WebConfig extends WebMvcConfigurerAdapter {
+	 
+	@Autowired
+	DataSource dataSource;
+	
+	@Bean
+	public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");// can change to PostgreSQL
+		ds.setUrl("jdbc:mysql://localhost:3306/springmvc");
+		ds.setUsername("briyan");
+		ds.setPassword("mysql");
+		
+		return ds;
+	}
+	
+	@Bean
+	public CommonsMultipartResolver getResolver() {
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		commonsMultipartResolver.setMaxInMemorySize(20*1024*1024);//20mb
+		
+		return commonsMultipartResolver;
+	}
+	
+	@Bean
+	public void addResourceHandlers(ResourceHandlerRegistration registration) {
+		registration.addResourceLocations("/resources/**/").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSouce() {
+		ResourceBundleMessageSource rb = new ResourceBundleMessageSource();
+		rb.setBasenames(new String[]{ "validation" });
+		
+		return rb;
+	}
+	
+}

25552230dbe01db95e2d91f3f10cb594fc5413e2
Briyan Adityatama <briyansykes@yahoo.com>
2018-09-20 18:13:52 +0700

    fix FileUploadController.java

--- src/main/java/springmvc/controller/FileUploadController.java
+++ src/main/java/springmvc/controller/FileUploadController.java
@@ -5,13 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import springmvc.model.FileUpload;
+
 @Controller
 public class FileUploadController {
 	
 	@RequestMapping(value="uploadPage", method=RequestMethod.GET)
 	public ModelAndView uploadPage() {
 		ModelAndView model = new ModelAndView("upload_page");
-		
+		FileUpload formUpload = new FileUpload();
+		model.addObject("formUpload", formUpload);
 		
 		return model;
 	}

6245103abdfee1baa1fe9691392e27529d1ee587
Briyan Adityatama <briyansykes@yahoo.com>
2018-09-21 11:32:40 +0700

    add main package & class UploadApplication.java

---
+++ src/main/java/com/springmvc/middleware/upload/UploadApplication.java
@@ -0,0 +1,17 @@
+package com.springmvc.middleware.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@SpringBootConfiguration
+public class UploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springmvc/middleware/upload/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author macOS
+ *
+ */
+package com.springmvc.middleware.upload;
